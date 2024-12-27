510ec309143a381be08ba9ffcd8fac4dc6a97f12
Kyle Loveless <kjloveless321@gmail.com>
2024-12-27 09:10:45 -0600

    begin working on lc3 vm

---
+++ README.md
@@ -0,0 +1,1 @@
+[Write your Own Virtual Machine - LC3](https://www.jmeiners.com/lc3-vm/)
---
+++ main.c
@@ -0,0 +1,4 @@
+/* Memory Storage */
+
+#define MEMORY_MAX (1 << 16)
+uint16_t memory[MEMORY_MAX];    /* 65536 locations */
